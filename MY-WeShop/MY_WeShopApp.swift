12b321e164d93a3a9f4e278444276f356c66c204
Zaid Neurothrone <54468049+neurothrone@users.noreply.github.com>
2022-10-24 09:09:02 +0200

    Initial Commit

---
+++ MY-WeShop/MY_WeShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  MY_WeShopApp.swift
+//  MY-WeShop
+//
+//  Created by Zaid Neurothrone on 2022-10-24.
+//
+
+import SwiftUI
+
+@main
+struct MY_WeShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
